5114b8e502d63eb79d3336f5b3d560b09007ff2a
wtlysh <2840310962@qq.com>
2020-07-23 20:05:15 +0800

    文章管理及富文本编辑器

--- admin/src/router.js
+++ admin/src/router.js
@@ -11,8 +11,8 @@ import ItemList from './views/ItemList.vue'
 import HeroEdit from './views/HeroEdit.vue'
 import HeroList from './views/HeroList.vue'
 
-// import ArticleEdit from './views/ArticleEdit.vue'
-// import ArticleList from './views/ArticleList.vue'
+import ArticleEdit from './views/ArticleEdit.vue'
+import ArticleList from './views/ArticleList.vue'
 
 // import AdEdit from './views/AdEdit.vue'
 // import AdList from './views/AdList.vue'
@@ -72,9 +72,19 @@ const router = new Router({
                     component: HeroList
                 },
 
-                //     { path: '/articles/create', component: ArticleEdit },
-                //     { path: '/articles/edit/:id', component: ArticleEdit, props: true },
-                //     { path: '/articles/list', component: ArticleList },
+                {
+                    path: '/articles/create',
+                    component: ArticleEdit
+                },
+                {
+                    path: '/articles/edit/:id',
+                    component: ArticleEdit,
+                    props: true
+                },
+                {
+                    path: '/articles/list',
+                    component: ArticleList
+                },
 
                 //     { path: '/ads/create', component: AdEdit },
                 //     { path: '/ads/edit/:id', component: AdEdit, props: true },
